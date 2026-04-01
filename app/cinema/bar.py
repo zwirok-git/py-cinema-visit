4393790a97849529a0a8efc94699154e5ea3ea31
Oleksii Prokopenko <oleksijprokopenko@gmail.com>
2026-04-01 21:02:32 +0200

    Update bar.py

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -2,9 +2,6 @@ from app.people.customer import Customer
 
 
 class CinemaBar:
-    def __init__(self) -> None:
-        pass
-
     @staticmethod
     def sell_product(customer: Customer, product: str) -> None:
         print(f"Cinema bar sold {product} to {customer.name}.")
